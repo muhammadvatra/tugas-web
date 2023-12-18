61cf9407468f176899379d061eb8eef26b7d9222
Muhammad Vatra Hidayatullah <mvatra341@gmail.com>
2023-12-18 16:45:26 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <a href="#">Made in</a>
+            <a href="#">By</a>
+            <a href="#">Vatra</a>
+            <a href="#">(221240001278)</a>
+            <a href="#">DB</a>
+        </div>
+    </div>
+</div>
